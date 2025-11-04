06258de83b58143c91b3f5ecb27e2b57e7f2ef11
Wayne-Thornberry <way-net@outlook.com>
2025-11-04 14:24:06 +0000

    Fix formatting and add missing import in configuration files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { copyFileSync } from 'fs'
 
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig({ 
   base: './',
+    plugins: [vue()],
   build: {
     outDir: 'dist'
   }
